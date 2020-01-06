8ae46484e7a9e73a1d80f8fa7024e13e3476f313
Kent C. Dodds <kent+github@doddsfamily.us>
2020-01-06 14:18:40 -0700

    Update data.js

--- data.js
+++ data.js
@@ -29,9 +29,9 @@ const pages = [
   },
   {
     name: 'Kent C. Dodds',
-    description: 'JavaScript Software Engineer, speaker, and trainer',
+    description: 'JavaScript Software Engineer, Educator, Live Streamer, Open Sourcerer',
     url: 'https://kentcdodds.com/uses',
-    emoji: '🙌',
+    emoji: '😃',
     country: '🇺🇸',
     computer: 'apple',
     phone: 'android',
@@ -46,6 +46,7 @@ const pages = [
       'React',
       'Speaker',
       'Blogger',
+      'Swell Guy',
     ],
   },
   {
@@ -58,22 +59,6 @@ const pages = [
     computer: 'apple',
     phone: 'apple',
     tags: ['Engineer', 'Full Stack', 'JavaScript', 'Ruby'],
-  },
-  {
-    name: 'Andrew Healey',
-    description: 'Software Engineer, Writer, Learner!',
-    url: 'https://healeycodes.com/uses',
-    emoji: '🦉',
-    country: '🇬🇧',
-    computer: 'apple',
-    phone: 'apple',
-    tags: [
-      'Software Engineer',
-      'Full Stack',
-      'JavaScript',
-      'Python',
-      'Writer',
-    ],
   },
   {
     name: 'Scott Tolinski',
